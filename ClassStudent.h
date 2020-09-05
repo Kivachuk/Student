1417e9e041a6667bf513cbb9202e4f847508a07d
Viktor <kiva@list.ru>
2020-09-05 21:47:47 +0300

    Добавление пояснений

--- ClassStudent.h
+++ ClassStudent.h
@@ -19,13 +19,13 @@ public:
     string faculty; //Факультет
     string group; //Группа
     int stage; //Курс
-    int mark[5];
+    int mark[5]; //массив оценок
     bool study; //Учится?
     void SetAge(int age); //Установка занчения возраста
     int GetAge(); //Получение значения возраста
 private:
     int age; //Возраст
-    double Cmark(int mark[5])
+    double Cmark(int mark[5])  //метод подсчета средней оценки
     {
         double cmark = 0;
         for (int i = 0; i < 5; i++)
